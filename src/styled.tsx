c30c0f1f3d858ca9c102668c3bc472f34ebaca48
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-05-02 11:01:41 +0900

    Add: 공통 css style 파일 생성 - description 파일마다 적용

---
+++ src/styled.tsx
@@ -0,0 +1,10 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+export const descContainer = css`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
