d13a43be9d31a9e6a4fffc8e8cee331b412ce04a
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-05-31 23:19:44 +0900

    Chore: TransferList 파일 props 추가

--- src/features/TransferList/TransferListDescription.tsx
+++ src/features/TransferList/TransferListDescription.tsx
@@ -4,10 +4,13 @@ import { layout, feature, description } from "../../styled";
 import TransferListDemo from "./TransferListDemo";
 
 const TransferListDescription = () => {
+  const itemlist1 = ["item1", "item2", "item3", "item4", "item5"];
+  const itemlist2 = ["item6", "item7", "item8", "item9", "item10"];
+
   return (
     <section css={layout}>
       <div css={feature}>
-        <TransferListDemo />
+        <TransferListDemo itemlist1={itemlist1} itemlist2={itemlist2} />
       </div>
       <div css={description}></div>
     </section>
