62f273097e21a8d3b6e73f1a591888d6fdb20947
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-06-03 23:40:27 +0900

    Style: TransferList button 스타일 + Chore: Item 컴포넌트 input id 관리

--- src/features/TransferList/TransferListDescription.tsx
+++ src/features/TransferList/TransferListDescription.tsx
@@ -6,7 +6,9 @@ const TransferListDescription = () => {
   const leftTitle = "Card1";
   const rightTitle = "Card2";
   const leftItemlist = ["item1", "item2", "item3", "item4", "item5"];
+  // You have to make unique, inon-overlapping name.
   const rightItemlist = ["item6", "item7", "item8", "item9", "item10"];
+  // You have to make unique, inon-overlapping name.
 
   return (
     <section css={layout}>
