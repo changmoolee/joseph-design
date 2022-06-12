8c0a3d2363eceddc0083feb8d84f36fab075fe8d
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-06-12 22:04:25 +0900

    Style: 페이지마다 기능 설명 구체화 완료

--- src/features/SkeletonUI/SkeletonUIDescription.tsx
+++ src/features/SkeletonUI/SkeletonUIDescription.tsx
@@ -50,7 +50,33 @@ const SkeletonUIDescription = () => {
           </SkeletonUIDemo>
         </section>
       </div>
-      <div css={description}></div>
+      <div css={description}>
+        <h3>SkeletonUI</h3>
+        <strong>
+          SkeletonUI 는 마크업을 자식컴포넌트로 넣어 원하는 디자인을 구현할 수
+          있습니다.
+        </strong>
+        <p>
+          SkeletonUI 에 마크업을 자식컴포넌트로 넣어 원하는 디자인을 구현할 수
+          있습니다. <br />
+          미리 제작된 마크업을 SkeletonUI 의 자식 컴포넌트로 넣어 주면, 해당
+          마크업 모양의 SkeletonUI 가 구현됩니다.
+          <br />
+          <br />
+          {"ex)"}
+          <br />
+          <code>{`import {children} from "./ChildrenExample";`}</code>
+          <br />
+          <br />
+          <code>{`<SkeletonUIDemo>`}</code>
+          <br />
+          <code> {`{children}`}</code>
+          <br />
+          <code>{`</SkeletonUIDemo>`}</code>
+          <br />
+          <br />
+        </p>
+      </div>
     </section>
   );
 };
